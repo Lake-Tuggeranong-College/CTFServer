6db8abc8b11b19d59c950d009bce7cfc6e0e6702
Ryan Cather <ryan.cather@ed.act.edu.au>
2026-02-08 13:56:09 +1100

    Update sensitiveInformation.h

--- modules/_template-ESP32Feather/src/sensitiveInformation.h
+++ modules/_template-ESP32Feather/src/sensitiveInformation.h
@@ -1,3 +1,23 @@
+/*
+ * Contains any sensitive Infomration that you do not want published to Github.
+ * 
+ * The SSID and Password variables will need to be changed if you’re connecting to another Wireless Access Point (such as at home).
+ *
+ * This file is supposed to be in the .gitignore
+ * 
+ */
+
+
+// Wifi network
 const char* ssid = "gogogadgetnodes";       // Wifi Network Name
 const char* password = "st@rw@rs";  // Wifi Password
+
+// MQTT client name
+const char* mqttClient = "ESP32";
+
+// MQTT Topic
+const char* mqttTopic = "challenges/Windmill"; // It's worth noting that an ESP32 can subscribe to more than 1 topic
+
+// Replace with the MQTT broker IP address and port (default port for MQTT is 1883)
 const char* mqttServer = "192.168.68.105";  
+const int mqttPort = 1883;
