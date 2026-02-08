c33f6be26fdf856bd19cd29bd0b4738ddde4e961
Ryan Cather <ryan.cather@ed.act.edu.au>
2026-02-08 22:29:03 +1100

    updates

--- modules/devModule/src/sensitiveInformation.h
+++ modules/devModule/src/sensitiveInformation.h
@@ -5,6 +5,10 @@
  *
  * This file is supposed to be in the .gitignore
  * 
+ * mqttClient NEEDS to match the 'ModuleName' column of the database row for this ESP32, and mqttTopic NEEDS to match the 'Module' column of the database row for this ESP32.
+ * 
+ * For instance, if mqttClient is "Windmill"
+ * then mqttTopic should be "challenges/Windmill"
  */
 
 
@@ -13,10 +17,12 @@ const char* ssid = "CyberRange";       // Wifi Network Name
 const char* password = "CyberRange";  // Wifi Password
 
 // MQTT client name
-const char* mqttClient = "ESP32";
+const char* mqttClient = "Windmill"; // This should be unique for each ESP32, e.g: "ESP32_Servo", "ESP32_Piezo", etc
 
 // MQTT Topic
-const char* mqttTopic = "challenges/Windmill"; // It's worth noting that an ESP32 can subscribe to more than 1 topic
+// const char* mqttTopic = "challenges/Windmill"; // It's worth noting that an ESP32 can subscribe to more than 1 topic
+const char* mqttTopic; 
+
 
 // Replace with the MQTT broker IP address and port (default port for MQTT is 1883)
 const char* mqttServer = "192.168.1.10";  
