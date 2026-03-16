3b06fe7857114ada1d494f4721f2dcb7ee95623a
9949747 <9949747@schoolsnet.act.edu.au>
2026-03-16 14:44:39 +1100

    I.P update

--- _modules/aeroplane_light_ARDUINO_THINGY/src/sensitiveInformation.h
+++ _modules/aeroplane_light_ARDUINO_THINGY/src/sensitiveInformation.h
@@ -19,5 +19,5 @@ const char* mqttClient = "aeroplane_light_ARDUINO_THINGY";
 const char* mqttTopic = "challenges/aeroplane_light"; // It's worth noting that an ESP32 can subscribe to more than 1 topic
 
 // Replace with the MQTT broker IP address and port (default port for MQTT is 1883)
-const char* mqttServer = "192.168.1.10";  
+const char* mqttServer = "192.168.1.116";  
 const int mqttPort = 1883;
