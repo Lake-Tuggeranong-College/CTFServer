758196903a29b276ec5759bdcf28b2cef834f974
rhhhys <0328198@schoolsnet.act.edu.au>
2026-09-14 14:46:17 +1000

    continued work on train data publisher page. more work is required

--- pages/challenges/trainDataPublisher.php
+++ pages/challenges/trainDataPublisher.php
@@ -2,11 +2,21 @@
 ob_start();
 require_once "../../includes/template.php";
 
-$command = "mosquitto_pub -h CTF-MQTT-Broker -t 'challenges/Train' -m 0"; 
-$currentData = shell_exec($command);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $stmt = $conn->prepare("UPDATE Challenges SET moduleValue = :val WHERE challengeTitle = :name");
+    $stmt->execute([':val' => $_POST['train_active'] ? 1 : 0, ':name' => 'The Train']);
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <h1>refresh this page to publish a packet to the train topic</h1>
+    <div class="text-center" style = "margin-top: 70px;">
+        <h1>emergency stop</h1>
+        <p>the train is currently <?php echo $_POST["train_active"] == true ? "running" : "stopped"; ?></p>
+        <p><?php echo $_POST["train_active"]?></p>
+        <form method="post">
+            <input type="hidden" name="train_active" value="false">
+            <button type="submit">stop train</button>
+        </form>
+    </div>
 </html>
