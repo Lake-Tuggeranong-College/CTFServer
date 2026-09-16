a651c5ea01b10f541dd44f9d2d12c2691c68c6d3
rhhhys <0328198@schoolsnet.act.edu.au>
2026-09-16 10:10:16 +1000

    finished train challenge page

--- pages/challenges/trainDataPublisher.php
+++ pages/challenges/trainDataPublisher.php
@@ -2,9 +2,12 @@
 ob_start();
 require_once "../../includes/template.php";
 
+$_POST["train_stopped"] = $_POST["train_stopped"] ?? "true";
+$train_bool = $_POST["train_stopped"] == "true" ? 0 : 1;
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt = $conn->prepare("UPDATE Challenges SET moduleValue = :val WHERE challengeTitle = :name");
-    $stmt->execute([':val' => $_POST['train_active'] ? 1 : 0, ':name' => 'The Train']);
+    $stmt->execute([":val" => $train_bool, ":name" => "Train"]);
 }
 ?>
 
@@ -12,10 +15,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="en">
     <div class="text-center" style = "margin-top: 70px;">
         <h1>emergency stop</h1>
-        <p>the train is currently <?php echo $_POST["train_active"] == true ? "running" : "stopped"; ?></p>
-        <p><?php echo $_POST["train_active"]?></p>
+        <p>the train is currently <?php echo $_POST["train_stopped"] == "true" ? "stopped" : "running"; ?></p>
         <form method="post">
-            <input type="hidden" name="train_active" value="false">
+            <input type="hidden" name="train_stopped" value="true">
             <button type="submit">stop train</button>
         </form>
     </div>
